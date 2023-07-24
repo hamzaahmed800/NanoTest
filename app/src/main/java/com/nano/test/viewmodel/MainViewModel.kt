a590b00f29ae9435a1e6c1caaaf540f421ca358e
Hamza Ahmed <hamzaahmed800@gmail.com>
2023-07-25 02:11:06 +0500

    app architecture setup viewmodels

--- app/src/main/java/com/nano/test/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/nano/test/viewmodel/MainViewModel.kt
@@ -1,8 +1,21 @@
 package com.nano.test.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.nano.test.Nano
+import com.nano.test.R
+import com.nano.test.data.model.product.AllProductResponse
+import com.nano.test.data.model.product.SingleProductResponse
 import com.nano.test.repository.MainRepository
+import com.nano.test.util.Event
+import com.nano.test.util.InternetUtil
+import com.nano.test.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,81 +24,132 @@ class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository
 ) : BaseViewModel(app){
 
-//    private val _dashboardResponse = MutableLiveData<Event<Resource<DashboardResponse>>>()
-//    val dashboardResponse: LiveData<Event<Resource<DashboardResponse>>>
-//        get() = _dashboardResponse
-//
-//    private val _saleTrendingResponse = MutableLiveData<Event<Resource<HourlyNanoResponse>>>()
-//    val saleTrendingResponse: LiveData<Event<Resource<HourlyNanoResponse>>>
-//        get() = _saleTrendingResponse
-//
-//    fun dashboardData(body: RequestBodies.DashboardBody) = viewModelScope.launch {
-//        dashboard(body)
-//    }
-//    private suspend fun dashboard(body: RequestBodies.DashboardBody) {
-//        _dashboardResponse.postValue(Event(Resource.Loading()))
-//        try {
-//            if (InternetUtil.hasInternetConnection(getApplication<Nano>())) {
-//                dashboardRepository.dashboard(body).collect { response ->
-//                    if(response.body() != null && response.body()!!.status == UNAUTHORIZED){
-//                        _dashboardResponse.postValue(
-//                            Event(
-//                                Resource.TokenExpire(
-//                                    response.body()!!.message,null
-//                                )
-//                            )
-//                        )
-//                    }else{
-//                        _dashboardResponse.postValue(handleLoginResponse(response))
-//                    }
-//                }
-//            } else {
-//                _dashboardResponse.postValue(
-//                    Event(
-//                        Resource.Error(
-//                            getApplication<Nano>().getString(
-//                                R.string.no_internet_connection
-//                            )
-//                        )
-//                    )
-//                )
-//            }
-//        } catch (t: Throwable) {
-//            when (t) {
-//                is IOException -> {
-//                    _dashboardResponse.postValue(
-//                        Event(
-//                            Resource.Error(
-//                                getApplication<Nano>().getString(
-//                                    R.string.network_failure
-//                                )
-//                            )
-//                        )
-//                    )
-//                }
-//                else -> {
-//                    _dashboardResponse.postValue(
-//                        Event(
-//                            Resource.Error(
-//                                getApplication<Nano>().getString(
-//                                    R.string.conversion_error
-//                                )
-//                            )
-//                        )
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//    private fun handleHomeResponse(response: Response<DashboardResponse>): Event<Resource<DashboardResponse>>? {
-//        if (response.isSuccessful) {
-//            response.body()?.let { resultResponse ->
-//                return Event(Resource.Success(resultResponse))
-//            }
-//        }
-//        return Event(Resource.Error(response.message()))
-//    }
+    private val _allProductResponse = MutableLiveData<Event<Resource<AllProductResponse>>>()
+    val allProducts: LiveData<Event<Resource<AllProductResponse>>>
+        get() = _allProductResponse
+
+    private val _singleProductResponse = MutableLiveData<Event<Resource<SingleProductResponse>>>()
+    val singleProductResponse: LiveData<Event<Resource<SingleProductResponse>>>
+        get() = _singleProductResponse
+
+    fun getAllProducts() = viewModelScope.launch {
+        allProducts()
+    }
+
+    fun getProductDetails(productId: String) = viewModelScope.launch {
+        singleProduct(productId)
+    }
+
+    private suspend fun allProducts() {
+        _allProductResponse.postValue(Event(Resource.Loading()))
+        try {
+            if (InternetUtil.hasInternetConnection(getApplication<Nano>())) {
+                mainRepository.getAllProducts().collect { response ->
+                    _allProductResponse.postValue(handleAllProductsResponse(response))
+                }
+            } else {
+                _allProductResponse.postValue(
+                    Event(
+                        Resource.Error(
+                            getApplication<Nano>().getString(
+                                R.string.no_internet_connection
+                            )
+                        )
+                    )
+                )
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> {
+                    _allProductResponse.postValue(
+                        Event(
+                            Resource.Error(
+                                getApplication<Nano>().getString(
+                                    R.string.network_failure
+                                )
+                            )
+                        )
+                    )
+                }
+                else -> {
+                    _allProductResponse.postValue(
+                        Event(
+                            Resource.Error(
+                                getApplication<Nano>().getString(
+                                    R.string.conversion_error
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private suspend fun singleProduct(productId: String) {
+        _singleProductResponse.postValue(Event(Resource.Loading()))
+        try {
+            if (InternetUtil.hasInternetConnection(getApplication<Nano>())) {
+                mainRepository.getSingleProduct(productId).collect { response ->
+                    _singleProductResponse.postValue(handleSingleProductsResponse(response))
+                }
+            } else {
+                _singleProductResponse.postValue(
+                    Event(
+                        Resource.Error(
+                            getApplication<Nano>().getString(
+                                R.string.no_internet_connection
+                            )
+                        )
+                    )
+                )
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> {
+                    _singleProductResponse.postValue(
+                        Event(
+                            Resource.Error(
+                                getApplication<Nano>().getString(
+                                    R.string.network_failure
+                                )
+                            )
+                        )
+                    )
+                }
+                else -> {
+                    _singleProductResponse.postValue(
+                        Event(
+                            Resource.Error(
+                                getApplication<Nano>().getString(
+                                    R.string.conversion_error
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+        }
+    }
+    private fun handleAllProductsResponse(response: Response<AllProductResponse>)
+            : Event<Resource<AllProductResponse>>? {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Event(Resource.Success(resultResponse))
+            }
+        }
+        return Event(Resource.Error(response.message()))
+    }
+
+    private fun handleSingleProductsResponse(response: Response<SingleProductResponse>)
+            : Event<Resource<SingleProductResponse>>? {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Event(Resource.Success(resultResponse))
+            }
+        }
+        return Event(Resource.Error(response.message()))
+    }
 
     override fun onCleared() {
         super.onCleared()
